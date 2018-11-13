286aab3a8006a468ef8292f8adb6128bd82c4853
Sue <ssica_789@163.com>
2018-11-13 16:10:56 +0800

    Ayyay -- find, contains

--- Array/src/com/company/SueArray.java
+++ Array/src/com/company/SueArray.java
@@ -87,8 +87,15 @@ public class SueArray {
     //check if e exists in this array, and return its index
     //if e does not exist, return -1
     public int find(int e){
-        
+        for(int i = 0; i < size; i++){
+            if (data[i] == e)
+                return i;
+        }
+        return -1;
     }
+
+
+    
 
 
     @Override
